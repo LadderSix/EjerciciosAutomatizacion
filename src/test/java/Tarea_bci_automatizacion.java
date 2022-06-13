9ef53d8fa9f294ac37e5ca7fa0bb06cb709b115a
Matias Rojas <Matias.Rojas@tsoftglobal.com>
2022-06-13 00:03:36 -0400

    Tarea automatizacion terminada 13-06

--- src/test/java/Tarea_bci_automatizacion.java
+++ src/test/java/Tarea_bci_automatizacion.java
@@ -1,4 +1,6 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -66,18 +68,56 @@ public class Tarea_bci_automatizacion {
 
         By birthdayYear = By.xpath("//*[@id=\"year\"]");
         driver.findElement(birthdayYear).sendKeys("1992");
-        Thread.sleep(1000);
 
-        WebElement masculino = driver.findElement(By.xpath("//*[@id=\"gender_option_male\"]"));
-        WebDriverWait wait = new WebDriverWait(driver,30);
-
-        if(masculino.isEnabled()){
-            wait.until(ExpectedConditions.elementToBeClickable(masculino));
+        // Seleccion de genero masculino -> Radiobutton
+        //WebElement masculino = driver.findElement(By.xpath("//input[@id=\"gender_option_male\"]"));
+        /* WebDriverWait wait = new WebDriverWait(driver,200);
+        if(masculino.isDisplayed()){
+           // wait.until(ExpectedConditions.elementToBeClickable(masculino));
+            Thread.sleep(15000);
             masculino.click();
             System.out.println("presionado");
         } else {
             System.out.println("error");
+        }*/
+        //WebElement masculino = driver.findElement(By.cssSelector("#gender_option_male"));
+        //WebDriverWait wait = new WebDriverWait(driver, 30);
+        //wait.until(ExpectedConditions.elementToBeClickable(masculino));
+        //masculino.click();
+
+        Thread.sleep(2000);
+        WebElement masculino = driver.findElement(By.xpath("//input[@id=\"gender_option_male\"]"));
+        JavascriptExecutor executor =(JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", masculino);
+        System.out.println(masculino.isSelected());
+
+
+        Thread.sleep(2000);
+        WebElement input = driver.findElement(By.id("marketing-opt-checkbox"));
+        WebElement span = driver.findElement(By.xpath("//body/div[@id='__next']/main[1]/div[1]/div[1]/form[1]/div[6]/div[1]/label[1]/span[1]"));
+        boolean b = input.isSelected();
+        System.out.println(b);
+        if(!b){
+            span.click();
         }
+
+        Thread.sleep(2000);
+        WebElement input2 = driver.findElement(By.id("third-party-checkbox"));
+        WebElement span2 = driver.findElement(By.xpath("//body/div[@id='__next']/main[1]/div[1]/div[1]/form[1]/div[7]/div[1]/label[1]/span[1]"));
+        boolean b2 = input2.isSelected();
+        System.out.println(b);
+        if(!b2){
+            span2.click();
+        }
+
+        WebElement btnRegister = driver.findElement(By.xpath("//body/div[@id='__next']/main[1]/div[1]/div[1]/form[1]/div[9]/div[1]/button[1]"));
+        btnRegister.submit();
+
+        WebElement robotConfirm = driver.findElement(By.xpath("//body/div[@id='__next']/main[1]/div[1]/div[1]/form[1]/div[8]/div[2]"));
+        Assert.assertEquals("Confirma que no eres un robot.",robotConfirm.getText());
+
+
+
 
     }
 }
